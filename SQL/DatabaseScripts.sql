c11952b3aa1e875837d6930ef842361c5c1a5128
subin2210 <39548737+subin2210@users.noreply.github.com>
2020-07-11 22:44:22 +0100

    Adding TestJdbc java class

---
+++ SQL/DatabaseScripts.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE  IF NOT EXISTS `web_customer_trcustomeracker`;
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
